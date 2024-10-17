3c86890d8f119fbb2a820db9cfff580f1341c577
VadimG <vadm.gagarin@gmail.com>
2024-10-17 20:50:25 +0200

    added alias for yazi

--- modules/terminal/yazi/default.nix
+++ modules/terminal/yazi/default.nix
@@ -4,7 +4,24 @@
   environment = { systemPackages = with pkgs; [ zellij ]; };
 
   home-manager.users.${vars.user} = {
-    programs = { yazi = { enable = true; }; };
+    programs = {
+      yazi = { enable = true; };
+
+      bash = {
+        bashrcExtra = ''
+          c() {
+            local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+            yazi "$@" --cwd-file="$tmp"
+            if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+              builtin cd -- "$cwd"
+            fi
+            rm -f -- "$tmp"
+          }
+
+        '';
+
+      };
+    };
 
     xdg.configFile."yazi/config.toml" = {
       enable = true;
